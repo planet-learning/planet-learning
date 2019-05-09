38657bfdf73c05285de07932c936344396bffc44
Proxyma <44854391+proxyma-centauri@users.noreply.github.com>
2019-05-09 20:13:40 +0200

    Feat : added printing of interesting stats of the database

--- planet-learning/catascript/catascript.py
+++ planet-learning/catascript/catascript.py
@@ -128,6 +128,26 @@ def add_entry_to_database(value_fields_dict):
         pass
     session.close
 
+#Statistics displaying
+def display_stats():
+    """
+    Displays the total number of entries in the database, and the total number of entries having an exoplanet already confirmed
+    """
+    #Queries
+    session = Session()
+
+    nb_total_entries = session.query(Catalog).count()
+    nb_total_already_confirmed = session.query(Catalog).filter(Catalog.already_confirmed == True).count()
+
+    session.close()
+
+    #Displaying
+    logging.info("     ~~~      ")
+    logging.info("NUMBER OF ENTRIES : {}".format(nb_total_entries))
+    logging.info("NUMBER OF ALREADY CONFIRMED : {}".format(nb_total_already_confirmed))
+    logging.info("     ~~~      ")
+
+
 #Main function
 def catascript():
     """
@@ -188,6 +208,9 @@ def catascript():
         process_confirmed()
 
         logging.info("Done : catascript - no new computing performed")
+
+    #Logging stats about the database entries
+    display_stats()
 
 if __name__ == "__main__":
     catascript()
