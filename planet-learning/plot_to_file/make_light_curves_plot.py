b66bdc93e63d0c70afa39d8ce5b62d38d2e25ce3
Proxyma <44854391+proxyma-centauri@users.noreply.github.com>
2019-05-11 15:54:32 +0200

    Fix : rename and first try

---
+++ planet-learning/plot_to_file/make_light_curves_plot.py
@@ -0,0 +1,92 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+from .catascript.base import Base, Session
+from .catascript.models import Catalog
+
+def get_TICS_with_confirmed():
+    """
+    This functions queries the database and returns the TIC IDs that have a confirmed planet.
+
+    Returns
+    -------
+        - dict_TIC_IDs : dict
+            The dictionnary containing the (TIC ID, path to light curve) pairs.
+    """
+    #Query
+    session = Session()
+
+    query = session.query(Catalog).filter(Catalog.already_confirmed == True).all()
+
+    session.close()
+
+    #Creating the dict of data
+    dict_TIC_IDs = {}
+
+    for entry in query:
+        dict_TIC_IDs[entry.ID] = entry.path
+    
+    #Returning
+    return dict_TIC_IDs
+
+
+def make_and_save_light_curve(TIC, lc_path):
+    """
+    This function plots the light curve using matplotlib and saves the output to a svg file
+
+    Parameters
+    ----------
+        - TIC : int
+            The value of the TIC ID corresponding to the light curve we want to plot
+        - lc_path : string
+            The path to the ligth curve storage on the nfs
+    """
+    # Getting the file
+    fits_file = load(lc_path)
+    
+    # Opening the data
+    with fits.open(fits_file, mode="readonly") as hdulist:
+        tess_bjds = hdulist[1].data['TIME']
+        sap_fluxes = hdulist[1].data['SAP_FLUX']
+        pdcsap_fluxes = hdulist[1].data['PDCSAP_FLUX']
+    
+    # Plotting
+    fig, ax = plt.subplots()
+    ax.plot(tess_bjds, pdcsap_fluxes, 'ko') 
+
+    # Labeling the axes and defining a title for the figure.
+    fig.suptitle("WASP-126 b Light Curve - Sector 1")
+    ax.set_ylabel("PDCSAP Flux (e-/s)")
+    ax.set_xlabel("Time (TBJD)")
+
+    # Adjusting the left margin so the y-axis label shows up.
+    plt.subplots_adjust(left=0.15)
+
+    #Saving the file to svg
+    plt.savefig('{}_light_curve.svg'.format(TIC))
+
+def make_light_curves_plot():
+    """
+    This function builds the plot of all light curves and save them to jpg
+    """
+    #Get plotting boolean
+    need_to_plot = int(os.getenv("PLOT_TO_FILE"))
+
+    if need_to_plot:
+        logging.info("Processing : plotting to file light curves of TICS with confirmed planets")
+
+        #Create a dir if not existent
+        dict_TIC_IDs = get_TICS_with_confirmed()
+
+        #Plot to file all light curves
+        for (TIC, lc_path) in dict_TIC_IDs.items():
+            make_and_save_light_curve(TIC, lc_path)
+
+        #Display message
+        logging.info("Done")
+    
+    else:
+        logging.info("Not plotting to file light curves of TICS with confirmed planets")
+
